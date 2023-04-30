b520282780f0b73f36c0cee3f0364c01a17d4d5f
nguyenphuanbmt113 <nguyenphuanbmt113@gmail.com>
2023-05-01 00:06:52 +0700

    tag crud

--- src/modules/tag/tag.controller.ts
+++ src/modules/tag/tag.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+  constructor(private tagsService: TagService) {}
+
+  @Post('create')
+  createUser(@Body() data: any) {
+    return this.tagsService.createTag(data);
+  }
+}
--- src/modules/tag/tag.service.ts
+++ src/modules/tag/tag.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Tag from 'src/entities/tab.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  constructor(
+    @InjectRepository(Tag)
+    private tagRepo: Repository<Tag>,
+  ) {}
+
+  async createTag(data: any) {
+    const tag = await this.tagRepo.create(data);
+    await this.tagRepo.save(tag);
+    return tag;
+  }
+}
